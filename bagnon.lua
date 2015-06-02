390b182589999911d773134eb355e75aa66f3d89
David Lynch <kemayo@gmail.com>
2015-06-02 14:54:52 -0500

    Update bagnon display code

--- bagnon.lua
+++ bagnon.lua
@@ -2,38 +2,30 @@ local core = LibStub("AceAddon-3.0"):GetAddon("DropTheCheapestThing")
 
 if Bagnon then
     local ItemSlot = Bagnon.ItemSlot
-    local SetBorderQuality = ItemSlot.SetBorderQuality
+    local UpdateBorder = ItemSlot.UpdateBorder
     local r, g, b = GetItemQualityColor(0)
 
-    function ItemSlot:SetBorderQuality(...)
-        local link
-        if self.GetItemSlotInfo then
-            link = select(7, self:GetItemSlotInfo())
-        else
-            link = select(7, self:GetInfo())
-        end
+    function ItemSlot:UpdateBorder(...)
+        -- First, do the core bagnon behavior for stuff like new-item flashing
+        UpdateBorder(self, ...)
 
+        -- Now override if we have junk
+        local link = self:GetItem()
         if link then
             local id = tonumber(strmatch(link, 'item:(%d+)'))
             local bag, slot = self:GetBag(), self:GetID()
             local bagslot = core.encode_bagslot(bag, slot)
-            
             if core.slot_contents[bagslot] then
-                self.questBorder:Hide()
-                self.border:SetVertexColor(r, g, b, self:GetHighlightAlpha())
-                self.border:Show()
-                return
+                self:SetBorderColor(r, g, b)
             end
         end
-        
-        SetBorderQuality(self, ...)
     end
 
     core.RegisterCallback("Button", "Junk_Update", function()
         for _,frame in pairs(Bagnon.frames) do
             if frame.itemFrame then
                 -- bagnon creates these on first use
-                frame.itemFrame:UpdateEverything()
+                frame.itemFrame:ForAll('UpdateBorder')
             end
         end
     end)
